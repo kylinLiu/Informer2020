ce86173ed6d8eb0c08fa749f3ed090911f309d0d
Liumumu <247651305@qq.com>
2021-03-14 10:04:02 +0800

    Update exp_informer.py

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -175,7 +175,7 @@ class Exp_Informer(Exp_Basic):
                     outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
                 else:
                     outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
-
+                print("outputs:",outputs)
                 f_dim = -1 if self.args.features=='MS' else 0
                 batch_y = batch_y[:,-self.args.pred_len:,f_dim:].to(self.device)
                 loss = criterion(outputs, batch_y)
